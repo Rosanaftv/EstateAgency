8fc583b973c468fe50bd5983cb19fb55349923e5
Rosana <rosanartec@gmail.com>
2021-02-04 22:33:40 +0000

    first mvp done

--- public/agent-single.js
+++ public/agent-single.js
@@ -3,15 +3,159 @@ axios
 .get('http://localhost:3000/api/agents/me',
 { headers: { token: localStorage.getItem('token') } })
 .then(response => {
-  const agents = document.getElementById('profile')
-  agents.innerHTML = ""
-  console.log(response.data)
-  //response.data
-    const agent = document.createElement('li')
-    agent.innerHTML =  `Name: ${agent.name},  Email: ${agent.email}, telephone: ${agent.telephone}`
-    agents.appendChild(agent)
-  })
- 
+  const agent = response.data
+  const agentHTML = `
+  <div class="col-md-12 col-lg-8">
+    <div class="title-single-box">
+      <h1 class="title-single">${agent.name}</h1>
+      <span class="color-text-a">Estate Agent</span>
+    </div>
+    </div>
+    <div class="col-md-12 col-lg-4">
+    <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">Home</a>
+        </li>
+        <li class="breadcrumb-item">
+          <a href="#">Agents</a>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+        ${agent.name}
+        </li>
+      </ol>
+    </nav>
+    </div>
+    </div>
+    </div>
+    </section>
+  
+    <section class="agent-single">
+    <div class="container">
+    <div class="row">
+    <div class="col-sm-12">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="agent-avatar-box">
+          <img src="img/agent-4.jpg" alt="" class="agent-avatar img-fluid">
+        </div>
+      </div>
+      
+      <div class="col-md-5 section-md-t3">
+        <div class="agent-info-box">
+          <div class="agent-title">
+            <div class="title-box-d">
+              <h3 class="title-d">${agent.name}
+                <br></h3>
+            </div>
+          </div>
+          <div class="agent-content mb-3">
+            <p class="content-d color-text-a">
+              Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+              Vivamus suscipit tortor
+              eget felis porttitor volutpat. Vivamus suscipit tortor eget felis porttitor volutpat.
+            </p>
+            <div class="info-agents color-a">
+              <p>
+                <strong>Phone: </strong>
+                <span class="color-text-a">${agent.telephone}</span>
+              </p>
+              <p>
+                <strong>Email: </strong>
+                <span class="color-text-a">${agent.email}</span>
+              </p>
+              <p>
+                <strong>Email: </strong>
+                <span class="color-text-a">${agent.email}</span>
+              </p>
+              </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+    </div>
+  `
+  const grid = document.getElementById("agentSingle-grid")
+  grid.innerHTML = agentHTML
+
+  // const agentSingle = response.data.properties
+  // agentSingle.forEach(single => {
+  //   const grid = document.getElementById("agentSingle-grid")
+  //   const child = 
+  //   `<div class="col-md-12 col-lg-8">
+  //   <div class="title-single-box">
+  //     <h1 class="title-single">${single.name}</h1>
+  //     <span class="color-text-a">Estate Agent</span>
+  //   </div>
+  //   </div>
+  //   <div class="col-md-12 col-lg-4">
+  //   <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
+  //     <ol class="breadcrumb">
+  //       <li class="breadcrumb-item">
+  //         <a href="#">Home</a>
+  //       </li>
+  //       <li class="breadcrumb-item">
+  //         <a href="#">Agents</a>
+  //       </li>
+  //       <li class="breadcrumb-item active" aria-current="page">
+  //       ${single.name}
+  //       </li>
+  //     </ol>
+  //   </nav>
+  //   </div>
+  //   </div>
+  //   </div>
+  //   </section>
+  
+  //   <section class="agent-single">
+  //   <div class="container">
+  //   <div class="row">
+  //   <div class="col-sm-12">
+  //   <div class="row">
+  //     <div class="col-md-6">
+  //       <div class="agent-avatar-box">
+  //         <img src="img/agent-4.jpg" alt="" class="agent-avatar img-fluid">
+  //       </div>
+  //     </div>
+      
+  //     <div class="col-md-5 section-md-t3">
+  //       <div class="agent-info-box">
+  //         <div class="agent-title">
+  //           <div class="title-box-d">
+  //             <h3 class="title-d">${single.name}
+  //               <br></h3>
+  //           </div>
+  //         </div>
+  //         <div class="agent-content mb-3">
+  //           <p class="content-d color-text-a">
+  //             Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+  //             Vivamus suscipit tortor
+  //             eget felis porttitor volutpat. Vivamus suscipit tortor eget felis porttitor volutpat.
+  //           </p>
+  //           <div class="info-agents color-a">
+  //             <p>
+  //               <strong>Phone: </strong>
+  //               <span class="color-text-a">${single.telephone}</span>
+  //             </p>
+  //             <p>
+  //               <strong>Email: </strong>
+  //               <span class="color-text-a">${single.email}</span>
+  //             </p>
+  //             <p>
+  //               <strong>Email: </strong>
+  //               <span class="color-text-a">${single.email}</span>
+  //             </p>
+  //             </div>
+  //            </div>
+  //          </div>
+  //        </div>
+  //      </div>
+  //    </div>
+  //   </div>`
+  //   grid.innerHTML += child
+  // })
+})
   axios
   .get('http://localhost:3000/api/agents/properties/me',
     { headers: { token: localStorage.getItem('token') } })
--- public/agentprofile.js
+++ public/agentprofile.js
@@ -24,7 +24,7 @@ document.getElementById('addProperty').addEventListener('click', function () {
       { headers: { token: localStorage.getItem('token') } }
     )
     .then(response => {
-      alert("se ha creado una propiedad")
+      alert("You have added a property")
     })
     .catch(err => console.error(err))
 })
@@ -41,98 +41,108 @@ document.getElementById("button-edit").addEventListener("click", function () {
     description: document.getElementById('text').value,
     image: document.getElementById('image').value
   }
-  axios.put('http://localhost:3000/api/properties/${propertyid}',
+  // const property_id = document.getElementById("property_id").dataset
+  
+  axios.put(`http://localhost:3000/api/properties/${localStorage.getItem("property_id")}`,
   body,
   { headers: { token: localStorage.getItem('token') } })
   .then(response => {
-    response.data.properties
-   
+    alert("Property modified")
+    fetchProperties()
+    //window.location.reload()
   })
   .catch(err => console.error(err))
 })
 
-axios
-.get('http://localhost:3000/api/agents/properties/me',
-  { headers: { token: localStorage.getItem('token') } })
-  .then(response => {
-    const agentProp = response.data.properties
-    agentProp.forEach(prop => {
+function fetchProperties () {
+  axios
+  .get('http://localhost:3000/api/agents/properties/me',
+    { headers: { token: localStorage.getItem('token') } })
+    .then(response => {
+      const agentProp = response.data.properties
       const grid = document.getElementById("agentProp-grid")
-      const child = 
-      `<div class="col-md-4">
-          <div class="card-box-a card-shadow">
-            <div class="img-box-a">
-            <img src="img/${prop.image}" alt="" class="img-a img-fluid">
-            </div>
-            <div class="card-overlay">
-              <div class="card-overlay-a-content">
-                <div class="card-header-a">
-                  <h2 class="card-title-a">
-                    <a href="#">${prop.type}
-                      <br /> ${prop.location}</a>
-                  </h2>
-                </div>
-                <div class="card-body-a">
-                  <div class="price-box d-flex">
-                    <span class="price-a">price | ${prop.price}</span>
+      grid.innerHTML = ""
+      agentProp.forEach(prop => {
+        const child = document.createElement('div')
+        child.classList.add('col-md-4')
+        
+        child.innerHTML = 
+        `<div class="card-box-a card-shadow">
+              <div class="img-box-a">
+              <img src="img/${prop.image}" alt="" class="img-a img-fluid">
+              </div>
+              <div class="card-overlay">
+                <div class="card-overlay-a-content">
+                  <div class="card-header-a">
+                    <h2 class="card-title-a">
+                      <a href="#">${prop.type}
+                        <br /> ${prop.location}</a>
+                    </h2>
                   </div>
-                  <a href="property-single.html" class="link-a">Click here to view
-                    <span>></span>
-                  </a>
-                </div>
-                <div class="card-footer-a">
-                  <ul class="card-info d-flex justify-content-around">
-                    <li>
-                      <h4 class="card-info-title">Area</h4>
-                      <span>${prop.m2}
-                        <sup>m2</sup>
-                      </span>
-                    </li>
-                    <li>
-                      <h4 class="card-info-title">Beds</h4>
-                      <span>${prop.bedrooms}</span>
-                    </li>
-                    <li>
-                      <h4 class="card-info-title">Baths</h4>
-                      <span>${prop.bathrooms}</span>
-                    </li>
-                    <li>
-                      <h4 class="card-info-title">Property id</h4>
-                      <span>${prop.property_id}</span>
-                    </li>
-                  </ul>
+                  <div class="card-body-a">
+                    <div class="price-box d-flex">
+                      <span class="price-a">price | ${prop.price}</span>
+                    </div>
+                    <a href="property-single.html" class="link-a">Click here to view
+                      <span>></span>
+                    </a>
+                  </div>
+                  <div class="card-footer-a">
+                    <ul class="card-info d-flex justify-content-around">
+                      <li>
+                        <h4 class="card-info-title">Area</h4>
+                        <span>${prop.m2}
+                          <sup>m2</sup>
+                        </span>
+                      </li>
+                      <li>
+                        <h4 class="card-info-title">Beds</h4>
+                        <span>${prop.bedrooms}</span>
+                      </li>
+                      <li>
+                        <h4 class="card-info-title">Baths</h4>
+                        <span>${prop.bathrooms}</span>
+                      </li>
+                      <li>
+                        <h4 class="card-info-title">Property id</h4>
+                        <span>${prop.property_id}</span>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            </div>
-            <button data-delete="${prop._id}">Delete</button>
-            <button data-edit="${prop._id}">Edit</button>
-          </div>`
-          
-        grid.innerHTML += child
+              </div>
+              <button type="button" id="btn-delete-${prop._id}">Delete</button>
+              <button type="button" id="btn-edit-${prop._id}">Edit</button>`
+            
+          grid.appendChild(child)
+          document.getElementById(`btn-delete-${prop._id}`).addEventListener("click", function() {
+            axios.delete(`http://localhost:3000/api/properties/${prop._id}`, 
+            { headers: { token: localStorage.getItem('token') } })
+            .then(response => {
+              alert("Property deleted")
+              window.location.reload()
+            })
+            .catch(err => console.error(err))
+          })
+    
+          document.getElementById(`btn-edit-${prop._id}`).addEventListener("click", function() {
+            localStorage.setItem("property_id", prop._id)
+  
+            // document.getElementById('property-form').setAttribute("data-id", prop._id)
+            document.getElementById('property_id').value = prop.property_id
+            document.getElementById('type').value = prop.type
+            document.getElementById("location").value = prop.location
+            document.getElementById('size').value = prop.m2
+            document.getElementById('bathrooms').value = prop.bathrooms
+            document.getElementById('bedrooms').value = prop.bedrooms
+            document.getElementById('price').value = prop.price
+            document.getElementById('text').value = prop.text
+            document.getElementById('image').value = prop.image
+          })
+        })
       })
-    })
-            document.querySelector(`button[data-delete="${prop._id}"]`).addEventListener("click", function() {
-              // prop._id
-              axios.delete("http://localhost:3000/api/properties/${propertyId}", 
-              { headers: { token: localStorage.getItem('token') } })
-              .then(response => {
-                  response.data.properties
-              })
-              
-              .catch(err => console.error(err))
-            })
+}
 
-            document.querySelector(`button[data-edit="${prop._id}"]`).addEventListener("click", function() {
-              // prop
-              document.getElementById('property_id').value = prop.property_id
-              document.getElementById('type').value = prop.type
-              document.getElementById("location").value = prop.location
-              document.getElementById('size').value = prop.m2
-              document.getElementById('bathrooms').value = prop.bathrooms
-              document.getElementById('bedrooms').value = prop.bedrooms
-              document.getElementById('price').value = prop.price
-              document.getElementById('text').value = prop.text
-              document.getElementById('image').value = prop.image
-            })
+fetchProperties();
             
